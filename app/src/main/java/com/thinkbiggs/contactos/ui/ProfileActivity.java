96ecb441d28143f3e1b0dc1d488593e6c3773c7a
Nate <nate@thinkbiggs.com>
2015-03-27 11:18:24 -0300

    Profile page is now saving data and pushing back to Connection List

--- app/src/main/java/com/thinkbiggs/contactos/ui/ProfileActivity.java
+++ app/src/main/java/com/thinkbiggs/contactos/ui/ProfileActivity.java
@@ -1,49 +1,88 @@
 package com.thinkbiggs.contactos.ui;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.thinkbiggs.contactos.R;
 import com.thinkbiggs.contactos.database.DatabaseHandler;
 import com.thinkbiggs.contactos.model.Contact;
 
-import java.security.SignatureException;
-import java.util.List;
-
 public class ProfileActivity extends ActionBarActivity {
+
+    private EditText prfNameField; //Name
+    private EditText prfNotesField; //Notes
+    private EditText prfLDateField; //LastDate
+    private EditText prfNDateField; //NextDate
+    private EditText prfLNotesField; //LastNotes
+    private EditText prfNNotesField; //NextNotes
+
+    private Button mSaveButton;
+    private Button mBackButton;
+
+    final DatabaseHandler db = new DatabaseHandler(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        prfNameField = (EditText) findViewById(R.id.vFirstName);
+        prfNotesField = (EditText) findViewById(R.id.vNotes);
+        prfLDateField = (EditText) findViewById(R.id.vLastDate);
+        prfNDateField = (EditText) findViewById(R.id.vNextDate);
+        prfLNotesField = (EditText) findViewById(R.id.vLastReport);
+        prfNNotesField = (EditText) findViewById(R.id.vNextReport);
+
+
+        //Code for getting details from intent passed byt ConnectionListActivity
         Intent intent = getIntent();
         String passed_id = intent.getStringExtra("contact_id");
         Toast.makeText(getBaseContext(), passed_id, Toast.LENGTH_LONG).show();
         //Upon receipt, pull up the object out of the database, based on ID, and populate the object here.
-        DatabaseHandler db = new DatabaseHandler(this);
 
-        //Create some test Contact objects
+        //Code for UPDATING contact that was already pulled over
 
-       // Contact nate = db.getContact(1);
-        //nate.setNotes("Changed Notes");
-        //List contacts = db.getAllContacts();
-        //db.updateContact(nate);
-       //List newContacts = db.getAllContacts();
-        //db.deleteContact(nate);
 
+        //Save Button for adding a new contact, IF contact was not pulled over
+        mSaveButton = (Button) findViewById(R.id.vSaveButton);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Contact contact = new Contact(
+                        prfNameField.getText().toString(), //Name
+                        prfNotesField.getText().toString(), //Notes
+                        prfLDateField.getText().toString(), //LastDate
+                        prfNDateField.getText().toString(), //NextDate
+                        prfLNotesField.getText().toString(), //LastNotes
+                        prfNNotesField.getText().toString() //NextNotes
+                );
+                db.addContact(contact);
+                Toast.makeText(getBaseContext(), contact.getFirstName() + " saved!", Toast.LENGTH_LONG).show();
+                backButton();
+            }
+        });
+
+        //Back Button. Just goes back, no saving
+        mBackButton = (Button) findViewById(R.id.vBackButton);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            backButton();
+            }
+        });
     }
 
+    private void backButton() {
+        Intent intent = new Intent(this, ConnectionListActivity.class);
+        startActivity(intent);
+
+    }
     /* WARNING ON BACK BUTTON
     public void showDialog(View view){
         AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
@@ -66,25 +105,4 @@ public class ProfileActivity extends ActionBarActivity {
 
     }*/
 
-
-    public void onShow(View view){
-        //We need to pull the data out of the fields in the activity, then call the database handler to store the data as a contact.
-        //Need Logic to determine if "Save" is ADDING or UPDATING.
-        //Need a DELETE button
-        /*
-        Contact contact = new Contact(
-                prfNameField, //Name
-                prfNotesField, //Notes
-                prfLDateField, //LastDate
-                prfNDateField, //NextDate
-                prfLNotesField, //LastNotes
-                prfNNotesField //NextNotes
-        );
-        DatabaseHandler db = new DatabaseHandler(this);
-        db.addContact(contact);
-        */
-        Toast toast = Toast.makeText(getApplicationContext(), "Contact was saved!", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0); //it sets the position of the pop-up message
-        toast.show();
-    }
 }
